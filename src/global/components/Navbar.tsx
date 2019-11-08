4dfeb30c93974d94eb4db362b1034d706ac45a06
unknown <vladi00345@gmail.com>
2019-11-08 01:22:31 -0800

    Added themes to navbar

--- src/global/components/Navbar.tsx
+++ src/global/components/Navbar.tsx
@@ -1,59 +1,36 @@
 import React from "react"
-import styled, {keyframes} from "styled-components"
-import {Link} from "react-router-dom"
+import {ThemeProvider} from "styled-components"
+import {NavList, NavItem, Underline, StyledLink} from "./StyledNavbar"
 
-const NavList = styled.ul`
-  display: flex;
-  list-style: none;
-  padding: 0;
-  font-size: 20px;
-  position: fixed;
-  right: 4%;
-  top: 20px;
-  color: #8c8c8c;
-`
+interface linkTheme {
+  color: string,
+  hoverColor: string
+}
 
-const NavItem = styled.li`
-  margin-right: 15px;
-  color: inherit;
-  :hover {
-    color: grey;
-    cursor: pointer;
-  }
-`
+const darkTheme: linkTheme = {
+  color: "#4f6d7a",
+  hoverColor: "#2d3b47"
+}
 
-const hoverAnim = keyframes`
-  from {
-    width: 0;
-  } to {
-    width: 100%;
-  }`
-
-const StyledLink = styled(Link)`
-  text-decoration: none;
-  color: inherit;
-`
-
-const Underline = styled.hr`
-  display: none;
-  color: inherit;
-  ${NavItem}:hover &{
-    display: block;
-    animation: ${hoverAnim} 300ms linear forwards;
-  }`
+const lightTheme: linkTheme = {
+  color: "#C0D6DF",
+  hoverColor: "#98a8b5"
+}
 
 const Navbar = () => {
   return (
-    <NavList>
-      <NavItem>
-        <StyledLink to="/">Portfolio</StyledLink>
-        <Underline/>
-      </NavItem>
-      <NavItem>
-        Resume
-        <Underline/>
-      </NavItem>
-    </NavList>
+    <ThemeProvider theme={darkTheme}>
+      <NavList>
+        <NavItem>
+          <StyledLink to="/">Portfolio</StyledLink>
+          <Underline/>
+        </NavItem>
+        <NavItem>
+          Resume
+          <Underline/>
+        </NavItem>
+      </NavList>
+    </ThemeProvider>
   )
 }
 
---
+++ src/global/components/StyledNavbar.ts
@@ -0,0 +1,44 @@
+import styled, {keyframes} from "styled-components"
+import {Link} from "react-router-dom"
+
+const NavList = styled.ul`
+  display: flex;
+  list-style: none;
+  padding: 0;
+  font-size: 20px;
+  position: fixed;
+  right: 4%;
+  top: 20px;
+  color: #8c8c8c;
+`
+
+const NavItem = styled.li`
+  margin-right: 15px;
+  color: inherit;
+  :hover {
+    color: grey;
+    cursor: pointer;
+  }
+`
+
+const hoverAnim = keyframes`
+  from {
+    width: 0;
+  } to {
+    width: 100%;
+  }`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+`
+
+const Underline = styled.hr`
+  display: none;
+  color: inherit;
+  ${NavItem}:hover &{
+    display: block;
+    animation: ${hoverAnim} 300ms linear forwards;
+  }`
+
+export {NavList, NavItem, Underline, StyledLink}
